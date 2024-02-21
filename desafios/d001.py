bb1356359dcef8f399ea7b3926beb6cf72fbc0ca
colacco <158360018+colacco@users.noreply.github.com>
2024-02-21 13:19:47 -0300

    primeiros exercícios

---
+++ desafios/d001.py
@@ -0,0 +1,5 @@
+print("===== DESAFIO 01 =====")
+nome = input('Qual é seu nome?')
+print('Olá ' + nome +"! Prazer em te conhecer!")
+print('A Diferença entre + e , está nos espaços. Com o +, o espaço fica mais encurtado, porém, coma virgula o espaço é maior')
+
---
+++ desafios/d003.py
@@ -0,0 +1,5 @@
+n1 = input('Digite um número')
+n2 = input('Digite outro número')
+print("A soma entre " + n1 + " e " + n2 + " é igual a " + n1 + n2 + ".")
+
+
