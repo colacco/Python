4e44f3c9cbb0437164634043ac0f8c1c8c6dc669
colacco <158360018+colacco@users.noreply.github.com>
2024-11-21 23:35:09 -0300

    new era

---
+++ reborn/variables.py
@@ -0,0 +1,18 @@
+#Variables are containers with a value. This value is like "x" in Algebra
+
+real_name = "Gabriel" #String
+user_name = "Hamura" #String
+age = 19
+age += 11
+
+print(type(real_name))
+print("Hello "+real_name+ " your username is "+user_name)
+
+print("Your age is: "+str(age))
+print(type(age))
+
+human = True
+woman = False
+print("You are a human: "+str(human))
+print("You are a woman: "+str(woman))
+print(type(human))
