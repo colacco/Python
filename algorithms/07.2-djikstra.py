1bed1d58b65fa0115b761b38c89d41990222a174
colacco <158360018+colacco@users.noreply.github.com>
2025-01-30 20:23:31 -0300

    more djikstra exercises

--- algorithms/07.2-djikstra.py
+++ algorithms/07.2-djikstra.py
@@ -26,4 +26,35 @@ costs["b"] = infinity
 costs["c"] = infinity
 costs["end"] = infinity
 
-# PARENTS
+# PARENTS:
+parents = {}
+parents["a"] = "start"
+parents["b"] = None
+parents["c"] = None
+parents["end"] = None
+
+processados = []
+
+def custo_baixo(costs):
+    nodo_barato = None
+    custo_barato = infinity
+    for nodo in costs:
+        cost = costs[nodo]
+        if cost < custo_barato and nodo not in processados:
+            custo_barato = cost
+            nodo_barato = nodo
+    return nodo_barato
+
+nodo = custo_baixo(costs)
+while nodo is not None:
+    vizinhos = grafos[nodo]
+    for n in vizinhos.keys():
+        novo_custo = costs[nodo] + vizinhos[n]
+        if novo_custo < costs[n]:
+            costs[n] = novo_custo
+            parents[n] = nodo
+    processados.append(nodo)
+    nodo = custo_baixo(costs)
+
+print(f"Esses são os pais {parents}")
+# I make almost all code, but there's something i forgot. The "processados" didn't have append, so the code would run forever... Like the book say, how this algorithm is considered important, I'm focusing to learn very well how to code and the reasoning of it.
---
+++ algorithms/07.3-djikstra.py
@@ -0,0 +1,61 @@
+# EXERCISE 7.1 - B
+
+#GRAPHS
+grafos = {}
+
+grafos["start"] = {}
+grafos["start"]["a"] = 2
+grafos["start"]["b"] = 2
+
+grafos["a"] = {}
+grafos["a"]["c"] = 2
+grafos["a"]["end"] = 2
+
+grafos["b"] = {}
+grafos["b"]["a"] = 2
+
+grafos["c"] = {}
+grafos["c"]["b"] = -1
+grafos["c"]["end"] = 2
+
+grafos["end"] = {}
+
+#Costs
+costs = {}
+costs["a"] = 2
+costs["b"] = 2
+costs["c"] = float("inf")
+costs["end"] = float("inf")
+
+#parents:
+parents = {}
+parents["a"] = "start"
+parents["b"] = "start"
+parents["c"] = None
+parents["d"] = None
+
+processados = []
+
+def next_nodo(costs):
+    custo_baixo = float("inf")
+    nodo_mais_baixo = None
+    for nodo in costs:
+        cost = costs[nodo]
+        if nodo < custo_baixo and nodo not in processados:
+            custo_baixo = cost
+            nodo_mais_baixo = nodo
+    return nodo_mais_baixo
+
+nodo = next_nodo(costs)
+while nodo is not None:
+    cost = costs[nodo]
+    vizinhos = grafos[nodo]
+    for n in vizinhos.keys():
+        novo_custo = cost + vizinhos[n]
+        if novo_custo < costs[n]:
+            costs[n] = novo_custo
+            parents[n] = nodo
+    processados.append(nodo)
+    nodo = next_nodo(costs)
+
+# Reading when the code is complete is so easy. But to start the reasoning by the zero is so difficult... May I need to make more reviews by this.
